0556bc2b11fc7e4a7bb23adda6cf82e4b6f32bec
Melissa Castro <melissa.anne.castro@gmail.com>
2019-07-31 18:57:19 -0400

    buttons with city names

--- giphy.js
+++ giphy.js
@@ -12,45 +12,49 @@
                     "Manila"
                 ]
 
+                //Buttons for original city list
+
+                for (let i = 0; i < cityList.length; i++) {
+                    console.log(cityList[i])
+                    var cityButton = $("<button>" + cityList[i] + "</button>");
+                    cityButton.appendTo("#buttons");
+                }
+
+
+
+                //Get gifs on button click from api
+
+                $("button").on("click", function () {
+
                     for (let i = 0; i < cityList.length; i++) {
-                        console.log(cityList[i])
+                    var city = (cityList[i])
+                    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + city +
+                        "&api_key=dOGBIKdSTDlKIjpGECqZggOdOEfD8WsT&limit=10";
+                        console.log(city)
                     }
 
+                    $.ajax({
+                            url: queryURL,
+                            method: "GET"
+                        })
+                        .then(function (response) {
+                            var results = response.data;
 
+                            for (var i = 0; i < results.length; i++) {
+                                var cityDiv = $("<div>");
 
+                                var rating = results[i].rating;
 
-                // $("#buttons").attr(cityButton)
+                                var p = $("<p>").text("Rating: " + rating);
 
-                // $("button").on("click", function () {
+                                var cityImage = $("<img>");
+                                cityImage.attr("src", results[i].images.fixed_height.url);
 
+                                cityDiv.prepend(cityImage);
+                                cityDiv.prepend(p);
 
-                //     var city = $(this).attr("data-city");
-                //     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + city +
-                //         "&api_key=dOGBIKdSTDlKIjpGECqZggOdOEfD8WsT&limit=10";
+                                $("#gifs-appear-here").prepend(cityDiv);
 
-                //     $.ajax({
-                //             url: queryURL,
-                //             method: "GET"
-                //         })
-                //         .then(function (response) {
-                //             console.log(response);
-                //             var results = response.data;
-
-                //             for (var i = 0; i < results.length; i++) {
-                //                 var cityDiv = $("<div>");
-
-                //                 var rating = results[i].rating;
-
-                //                 var p = $("<p>").text("Rating: " + rating);
-
-                //                 var cityImage = $("<img>");
-                //                 cityImage.attr("src", results[i].images.fixed_height.url);
-
-                //                 cityDiv.prepend(cityImage);
-                //                 cityDiv.prepend(p);
-
-                //                 $("#gifs-appear-here").prepend(cityDiv);
-
-                //             }
-                //         })
-                // });
+                            }
+                        })
+                });
