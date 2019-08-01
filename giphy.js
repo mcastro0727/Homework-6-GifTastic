cc9970ddb8901ae97f325c79153e1a69f6a99780
Melissa Castro <melissa.anne.castro@gmail.com>
2019-08-01 02:16:21 -0400

    city button generate gifs!

--- giphy.js
+++ giphy.js
@@ -1,60 +1,60 @@
-                
-                
-                var cityList = [
-                    "Las Vegas",
-                    "New York",
-                    "London",
-                    "Paris",
-                    "Rome",
-                    "Dublin",
-                    "Beijing",
-                    "Dubai",
-                    "Manila"
-                ]
+$(document).ready(function () {
 
-                //Buttons for original city list
+    var cityList = [
+        "Las Vegas",
+        "New York",
+        "London",
+        "Paris",
+        "Rome",
+        "Dublin",
+        "Beijing",
+        "Dubai",
+        "Manila"
+    ];
 
-                for (let i = 0; i < cityList.length; i++) {
-                    console.log(cityList[i])
-                    var cityButton = $("<button>" + cityList[i] + "</button>");
-                    cityButton.appendTo("#buttons");
+    //Buttons for city list
+    function makeButtons() {
+        $("#buttons").empty();
+        for (let i = 0; i < cityList.length; i++) {
+            console.log(cityList[i])
+            $("#buttons").append("<button class='btn btn-success' data-city='" + cityList[i] + "'>" + cityList[i] + "</button>")
+        }
+    }
+
+    makeButtons();
+
+    //Get gifs on button click from api
+
+    $("button").on("click", function () {
+
+            var city = $(this).attr("data-city");
+            var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + city +
+                "&api_key=dOGBIKdSTDlKIjpGECqZggOdOEfD8WsT&limit=10";
+            console.log(city);
+
+        $.ajax({
+                url: queryURL,
+                method: "GET"
+            })
+            .then(function (response) {
+                var results = response.data;
+
+                for (var i = 0; i < results.length; i++) {
+                    var cityDiv = $("<div>");
+
+                    var rating = results[i].rating;
+
+                    var p = $("<p>").text("Rating: " + rating);
+
+                    var cityImage = $("<img>");
+                    cityImage.attr("src", results[i].images.fixed_height.url);
+
+                    cityDiv.prepend(cityImage);
+                    cityDiv.prepend(p);
+
+                    $("#gifs-appear-here").prepend(cityDiv);
+
                 }
-
-
-
-                //Get gifs on button click from api
-
-                $("button").on("click", function () {
-
-                    for (let i = 0; i < cityList.length; i++) {
-                    var city = (cityList[i])
-                    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + city +
-                        "&api_key=dOGBIKdSTDlKIjpGECqZggOdOEfD8WsT&limit=10";
-                        console.log(city)
-                    }
-
-                    $.ajax({
-                            url: queryURL,
-                            method: "GET"
-                        })
-                        .then(function (response) {
-                            var results = response.data;
-
-                            for (var i = 0; i < results.length; i++) {
-                                var cityDiv = $("<div>");
-
-                                var rating = results[i].rating;
-
-                                var p = $("<p>").text("Rating: " + rating);
-
-                                var cityImage = $("<img>");
-                                cityImage.attr("src", results[i].images.fixed_height.url);
-
-                                cityDiv.prepend(cityImage);
-                                cityDiv.prepend(p);
-
-                                $("#gifs-appear-here").prepend(cityDiv);
-
-                            }
-                        })
-                });
+            })
+        })
+    })
