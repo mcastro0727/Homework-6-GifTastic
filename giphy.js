df3b49babd0e30ef24a11a884839768f6130db15
Melissa Castro <melissa.anne.castro@gmail.com>
2019-08-01 04:38:42 -0400

    added bootstrap and css elements

--- giphy.js
+++ giphy.js
@@ -1,3 +1,10 @@
+//TODO
+//Make new city buttons work to get gifs
+//Figure out why clear form doesn't work
+//Pause gifs
+
+
+
 $(document).ready(function () {
 
     var cityList = [
@@ -12,24 +19,40 @@ $(document).ready(function () {
         "Manila"
     ];
 
-    //Buttons for city list
+    //Buttons for original city list
     function makeButtons() {
         $("#buttons").empty();
         for (let i = 0; i < cityList.length; i++) {
             console.log(cityList[i])
-            $("#buttons").append("<button class='btn btn-success' data-city='" + cityList[i] + "'>" + cityList[i] + "</button>")
+            $("#buttons").append("<button class='btn btn-info' data-city='" + cityList[i] + "'>" + cityList[i] + "</button>")
         }
     }
 
     makeButtons();
 
+
+    // Add a button for city entered in form
+    $("#add-city").on("click", function () {
+        event.preventDefault();
+        var newCity = $("#city-input").val().trim();
+        cityList.push(newCity);
+        makeButtons();
+        clearForm();
+        console.log(newCity)
+    });
+
+    //To clear form - NOT WORKING
+    function clearForm() {
+        $("#city-input").empty();
+    }
+
+
     //Get gifs on button click from api
     $("button").on("click", function () {
 
-            var city = $(this).attr("data-city");
-            var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + city +
-                "&api_key=dOGBIKdSTDlKIjpGECqZggOdOEfD8WsT&limit=10";
-            console.log(city);
+        var city = $(this).attr("data-city");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + city +
+            "&api_key=dOGBIKdSTDlKIjpGECqZggOdOEfD8WsT&limit=10";
 
         $.ajax({
                 url: queryURL,
@@ -49,31 +72,34 @@ $(document).ready(function () {
                     var p = $("<p>").text("Rating: " + rating);
 
                     var cityImage = $("<img>");
+
                     cityImage.attr("src", results[i].images.fixed_height.url);
+                    cityImage.attr("data-still", results[i].images.fixed_height_still.url);
+                    cityImage.attr("data-animate", results[i].images.fixed_height.url);
+                    cityImage.attr("data-state", "still");
+                    cityImage.attr("class", "gif");
+
 
                     cityDiv.prepend(cityImage);
                     cityDiv.prepend(p);
 
                     $("#city-gifs").append(cityDiv);
 
-
                 }
             })
-        })
-
-	// Add a button for city entered in form
-	$("#add-city").on("click", function () {
-		event.preventDefault();
-		var city = $("#city-input").val().trim();
-		cityList.push(city);
-        makeButtons();
-        clearForm();
-    });
-    
-    function clearForm() {
-    $("#city-input").empty();
-    }
-
-
 
     })
+
+    //To pause gifs - NOT WORKING
+    $(".gif").on("click", function () {
+        var state = $(this).attr("data-state");
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+        } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
+    });
+
+});
