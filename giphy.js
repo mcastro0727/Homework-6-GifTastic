590b64914cecb6e1e0259f914db22a1f71f0a651
Melissa Castro <melissa.anne.castro@gmail.com>
2019-08-01 20:07:49 -0400

    new city buttons get gifsgit add -A

--- giphy.js
+++ giphy.js
@@ -1,6 +1,5 @@
 //TODO
-//Make new city buttons work to get gifs
-//Figure out why clear form doesn't work
+//Figure out why clearForm function doesn't work
 //Pause gifs
 
 
@@ -25,9 +24,49 @@ $(document).ready(function () {
         for (let i = 0; i < cityList.length; i++) {
             console.log(cityList[i])
             $("#buttons").append("<button class='btn btn-info' data-city='" + cityList[i] + "'>" + cityList[i] + "</button>")
+
+            $("button").on("click", function () {
+
+                var city = $(this).attr("data-city");
+                var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + city +
+                    "&api_key=dOGBIKdSTDlKIjpGECqZggOdOEfD8WsT&limit=10";
+                console.log(city)
+        
+                $.ajax({
+                        url: queryURL,
+                        method: "GET"
+                    })
+                    .then(function (response) {
+                        var results = response.data;
+        
+                        $("#city-gifs").empty();
+        
+                        for (var i = 0; i < results.length; i++) {
+                            var cityDiv = $("<div>");
+        
+                            var rating = results[i].rating;
+        
+                            var p = $("<p>").text("Rating: " + rating);
+        
+                            var cityImage = $("<img>");
+        
+                            cityImage.attr("src", results[i].images.fixed_height.url);
+                            cityImage.attr("data-still", results[i].images.fixed_height_still.url);
+                            cityImage.attr("data-animate", results[i].images.fixed_height.url);
+                            cityImage.attr("data-state", "still");
+                            cityImage.attr("class", "gif");
+        
+                            cityDiv.prepend(cityImage);
+                            cityDiv.prepend(p);
+        
+                            $("#city-gifs").append(cityDiv);
+        
+                        }
+                    })
+            })
+        
         }
     }
-
     makeButtons();
 
 
@@ -41,6 +80,7 @@ $(document).ready(function () {
         console.log(newCity)
     });
 
+
     //To clear form - NOT WORKING
     function clearForm() {
         $("#city-input").empty();
@@ -53,6 +93,7 @@ $(document).ready(function () {
         var city = $(this).attr("data-city");
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + city +
             "&api_key=dOGBIKdSTDlKIjpGECqZggOdOEfD8WsT&limit=10";
+        console.log(city)
 
         $.ajax({
                 url: queryURL,
@@ -62,7 +103,6 @@ $(document).ready(function () {
                 var results = response.data;
 
                 $("#city-gifs").empty();
-
 
                 for (var i = 0; i < results.length; i++) {
                     var cityDiv = $("<div>");
@@ -79,7 +119,6 @@ $(document).ready(function () {
                     cityImage.attr("data-state", "still");
                     cityImage.attr("class", "gif");
 
-
                     cityDiv.prepend(cityImage);
                     cityDiv.prepend(p);
 
@@ -87,7 +126,6 @@ $(document).ready(function () {
 
                 }
             })
-
     })
 
     //To pause gifs - NOT WORKING
@@ -96,10 +134,11 @@ $(document).ready(function () {
         if (state === "still") {
             $(this).attr("src", $(this).attr("data-animate"));
             $(this).attr("data-state", "animate");
-        } else {
+            console.log(this)
+        }
+        else {
             $(this).attr("src", $(this).attr("data-still"));
             $(this).attr("data-state", "still");
         }
     });
-
 });
